c1d6468a5a121b32d271ee3e000f5fbecc1fb524
Pokecris200 <59978098+Pokecris200@users.noreply.github.com>
2020-01-27 11:16:31 -0500

    Update Libreria1.py

--- Libreria1.py
+++ Libreria1.py
@@ -2,44 +2,54 @@ import math
 
 def Suma (a, b):
     return (a[0]+b[0], a[1]+ b[1])
+
 def prettyPrinting(a):
     if a[1]>=0 :
         print(a[0],"+",a[1],"i")
     else :
         print(a[0],a[1],"i")
+        
 def resta(a, b):
     return (a[0]-b[0], a[1]-b[1])
+
 def multiplicacion (a, b):
     First = (a[0]*b[0])-(a[1]*b[1])
     Second = (a[0]*b[1])+(a[1]*b[0])
     return(First, Second)
+
 def div (a,b):
     num1 = (a[0]*b[0])+(a[1]*b[1])
     den = (b[0]**2)+(b[1]**2)
     num2 =(b[0]*a[1])-(a[0]*b[1]) 
     return ((num1/den), (num2/den))
+
 def conj (a):
     Conjugado = (a[1] * (-1))
     return (a[0], Conjugado)
+
 def modulo (a):
     rad = (a[0]**2) + (a[1]**2)
     mod = math.sqrt (rad)
     return mod
+
 def Polar (a):
     f = (a[0]**2) + (a[1]**2)
     rho = math.sqrt (f)
     d = a[1] / a[0]
     Theta = math.atan (d)
     return (rho, Theta)
+
 def Fase (a):
     c = Polar (a)
     return c[1]
+
 def sumaVect(a,b):
     matriz = []
     i = 0
     for i in range(len(a)):
         matriz = matriz + [Suma (a[i],b[i])]
     return matriz
+
 def inversaVect(a):
     matriz = []
     i = 0
@@ -47,12 +57,14 @@ def inversaVect(a):
     for i in range(len(a)):
         matriz = matriz + [multiplicacion (a[i], s)]
     return matriz
+
 def productoEscalarV(a, b):
     matriz = []
     i = 0
     for i in range(len(a)):
         matriz = matriz + [multiplicacion (a[i], b)]
     return matriz
+
 def sumaMatrix (a, b):
     i = 0
     j = 0
@@ -66,15 +78,58 @@ def sumaMatrix (a, b):
                 c[i][j] = Suma (a[i][j], b[i][j])
     return c
 
-    
+def inversaMatrix(a):
+    n,m = len(a),len(a[0])
+    i = 0
+    j = 0
+    c = [[0 for j in range (m) ]for i in range (n)]
+    for i in range (n):
+            for j in range (m):
+                c[i][j] = multiplicacion (a[i][j], (-1,0))
+    return c
+
+def multiEscalarMatrix (a, b):
+    n,m = len(a),len(a[0])
+    i = 0
+    j = 0
+    c = [[0 for j in range (m) ]for i in range (n)]
+    for i in range (n):
+            for j in range (m):
+                c[i][j] = multiplicacion (a[i][j], b)
+    return c
+
+def matrixTrans (a):
+    n,m = len(a),len(a[0])
+    i = 0
+    j = 0
+    c = [[0 for j in range (m) ]for i in range (n)]
+    for i in range (n):
+            for j in range (m):
+                c[i][j] = a [j][i]
+    return c
+
+def matrixConj (a):
+    n,m = len(a),len(a[0])
+    i = 0
+    j = 0
+    c = [[0 for j in range (m) ]for i in range (n)]
+    for i in range (n):
+            for j in range (m):
+                c[i][j] = conj (a[i][j])
+    return c
+
+def matrixAdj (a):
+    c = matrixTrans (matrixConj (a))
+    return c
 
 
-w = [(7,3),(4,2)]
+mat1 = [[(7,3),(4,2)], [(6,1),(5,2)]]
+w = [[(7,3),(4,2)], [(6,1),(5,2)]]
 l = [(1,2),(3,5)]
-
 d =(3,4)
+#print(sumaMatrix(w,mat1))
 v =(-1,0)
-print(productoEscalarV(w,v))
+print(matrixAdj(w))
 #prettyPrinting(multiplicacion (d,v))
 c =(-1,0)
 g = (-1,0)
@@ -83,4 +138,4 @@ g = (-1,0)
 #y = (1,0)
 #prettyPrinting(multiplicacion (x,y))
 #prettyPrinting(Suma (multiplicacion (d,v),multiplicacion (c,g)))
-print(Fase (v))
+#print(Fase (v))
