177c42a3c52f82fc5e25addc9c3b3558f1cb4bc1
Pokecris200 <59978098+Pokecris200@users.noreply.github.com>
2020-02-12 16:17:11 -0500

    Update testLibreria1.py

--- testLibreria1.py
+++ testLibreria1.py
@@ -27,11 +27,17 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(modulo (a), 1.41)
     def testPolar(self):
         a = (1,1)
-        self.assertEqual(Polar (a), (1.41,0.78))
+        self.assertEqual(Polar (a), (1.41,0.79))
     def testFase (self):
         a = (1,1)
-        self.assertEqual(Fase (a), 0.78)
-
-
+        self.assertEqual(Fase (a), 0.79)
+    def test_sumaVect(self):
+        a = [(1,3),(2,5),(4,2),(3,3)]
+        b = [(2,2),(1,12),(5,1),(22,0)]
+        self.assertEqual(sumaVect (a, b), [(3, 5), (3, 17), (9, 3), (25, 3)])
+    def test_inversaVect(self):
+        a = [(4,3),(2,0),(9,2),(6,3)]
+        self.assertEqual(inversaVect (a), [(-4, -3), (-2, 0), (-9, -2), (-6, -3)])
+    
 if __name__ == '__main__':
     unittest.main()
