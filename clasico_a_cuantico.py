3286051729c72bd8e74271a9e2069e1f152bdc7d
Pokecris200 <59978098+Pokecris200@users.noreply.github.com>
2020-03-12 15:46:47 -0500

    Add files via upload

---
+++ clasico_a_cuantico.py
@@ -0,0 +1,24 @@
+from matplotlib import pyplot
+from Libreria1 import *
+import math
+def pot (matriz,exp):
+    for i in range (1,b):
+        res = multiMatrix(matriz,matriz)
+    return res
+
+def experimentos(a, b, c):
+    r1 = pot(a,c)
+    r2 = Accion(r1,b)
+    return(res2)
+def probabilidad (vector):
+    x,y = [],[]
+    for i in range (len(vector)):
+        a = vector[i]
+        y = y + [modulo(a[0],a[1])*100]
+    for i in range (len(vector)):
+        x = x + [i]
+def dibujo():
+    pyplot.title("PROBABILIDAD")
+    pyplot.bar(x,height=y)
+    pyplot.savefig("experimento.png")
+    pyplot.show()
