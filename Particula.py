063638e5bddf534d9d8fe58a2111c5b0acec28e2
Pokecris200 <59978098+Pokecris200@users.noreply.github.com>
2020-03-18 12:27:51 -0500

    Update Particula.py

--- Particula.py
+++ Particula.py
@@ -2,7 +2,7 @@ from Libreria1 import *
 def proba(a, pos):
     num = modulo(a[pos])
     den = norma(a)
-    por = num/den
+    por = (num**2)/(den**2)
     round(por,2)
     return por
 def transito(a,b):
